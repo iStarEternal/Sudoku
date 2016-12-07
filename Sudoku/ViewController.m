cfba27759985eee2903d81c17c08dd38aef111c6
星星老师 <576681253@qq.com>
2016-12-07 17:30:11 +0800

    Update ViewController.m

--- Sudoku/ViewController.m
+++ Sudoku/ViewController.m
@@ -24,6 +24,8 @@
 
 @interface ViewController ()
 
+@property (nonatomic, weak) UIView *containerView;
+
 @end
 
 
@@ -34,31 +36,115 @@
     
     UIView *containerView = [[UIView alloc] init];
     [self.view addSubview:containerView];
+    self.containerView = containerView;
     containerView.backgroundColor = [UIColor whiteColor];
     containerView.layer.borderWidth = 1;
     containerView.layer.borderColor = [UIColor grayColor].CGColor;
     
-    [containerView makeConstraints:^(MASConstraintMaker *make) {
+    
+    // 固定containerView的宽高
+    // 宫格大小随着containerView的宽高而改变
+    [self distributeDynamicCellWithCount:10 warp:3];
+    
+    // 固定containerView的宽
+    // 宫格的宽随containerView的宽改变
+    // 固定宫格的高
+    // containerView的高随宫格的高改变
+    // [self distributeDynamic2CellWithCount:13 warp:3];
+    
+    
+    // 固定container大小
+    // 固定宫格大小
+    // [self distributeFixedCellWithCount:17 warp:4];
+}
+
+
+
+// 固定containerView的宽高
+// 宫格大小随containerView的宽高改变
+- (void)distributeDynamicCellWithCount:(NSUInteger)count warp:(NSUInteger)warp {
+    
+    [self.containerView.subviews enumerateObjectsUsingBlock:^(__kindof UIView * _Nonnull obj, NSUInteger idx, BOOL * _Nonnull stop) {
+        [obj removeFromSuperview];
+    }];
+    
+    [self.containerView remakeConstraints:^(MASConstraintMaker *make) {
+        make.left.equalTo(15);
+        make.top.equalTo(66);
+        make.right.equalTo(-15);
+        make.height.equalTo(280);
+    }];
+    
+    for (int i = 0; i < count; i++) {
+        UIView *view = [[UIView alloc] init];
+        view.backgroundColor = [UIColor randomColor];
+        [self.containerView addSubview:view];
+    }
+    
+    [self.containerView.subviews mas_distributeSudokuViewsWithFixedItemWidth:0 fixedItemHeight:0
+                                                            fixedLineSpacing:10 fixedInteritemSpacing:20
+                                                                   warpCount:3
+                                                                  topSpacing:10 bottomSpacing:10 leadSpacing:10 tailSpacing:10];
+}
+
+// 固定containerView的宽
+// 宫格的宽随containerView的宽改变
+// 固定宫格的高
+// containerView的高随宫格的高改变
+- (void)distributeDynamic2CellWithCount:(NSUInteger)count warp:(NSUInteger)warp {
+    
+    [self.containerView.subviews enumerateObjectsUsingBlock:^(__kindof UIView * _Nonnull obj, NSUInteger idx, BOOL * _Nonnull stop) {
+        [obj removeFromSuperview];
+    }];
+    
+    [self.containerView remakeConstraints:^(MASConstraintMaker *make) {
+        make.left.equalTo(15);
+        make.top.equalTo(66);
+        make.right.equalTo(-15);
+    }];
+    
+    for (int i = 0; i < count; i++) {
+        UIView *view = [[UIView alloc] init];
+        view.backgroundColor = [UIColor randomColor];
+        [self.containerView addSubview:view];
+    }
+    
+    [self.containerView.subviews mas_distributeSudokuViewsWithFixedItemWidth:0 fixedItemHeight:80
+                                                            fixedLineSpacing:10 fixedInteritemSpacing:20
+                                                                   warpCount:3
+                                                                  topSpacing:10
+                                                               bottomSpacing:10 leadSpacing:10 tailSpacing:10];
+}
+
+
+// 固定container大小
+// 固定宫格大小
+- (void)distributeFixedCellWithCount:(NSUInteger)count warp:(NSUInteger)warp {
+    
+    [self.containerView.subviews enumerateObjectsUsingBlock:^(__kindof UIView * _Nonnull obj, NSUInteger idx, BOOL * _Nonnull stop) {
+        [obj removeFromSuperview];
+    }];
+    
+    [self.containerView remakeConstraints:^(MASConstraintMaker *make) {
         make.left.equalTo(15);
         make.top.equalTo(66);
         make.right.equalTo(-15);
         make.height.equalTo(300);
     }];
     
-    for (int i = 0; i < 10; i++) {
+    for (int i = 0; i < count; i++) {
         UIView *view = [[UIView alloc] init];
         view.backgroundColor = [UIColor randomColor];
-        [containerView addSubview:view];
+        [self.containerView addSubview:view];
     }
     
-    [containerView.subviews mas_distributeSudokuViewsWithFixedItemWidth:50 fixedItemHeight:50 warpCount:3 topSpacing:10 bottomSpacing:10 leadSpacing:10 tailSpacing:10];
     
-    [containerView.subviews mas_distributeSudokuViewsWithFixedItemWidth:0 fixedItemHeight:0 fixedLineSpacing:10 fixedInteritemSpacing:20 warpCount:3 topSpacing:10 bottomSpacing:10 leadSpacing:10 tailSpacing:10];
-    
+    [self.containerView.subviews mas_distributeSudokuViewsWithFixedItemWidth:50 fixedItemHeight:50
+                                                              warpCount:warp
+                                                             topSpacing:10 bottomSpacing:10 leadSpacing:10 tailSpacing:10];
 }
 
 @end
-
 
 
 
